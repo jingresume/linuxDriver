0cbbe3604d073ba724c602fd6cea0ea9e5428ab7
jing <2016192403@qq.com>
2024-03-09 20:44:09 +0800

    modify globalmem for multi device

---
+++ doc/API.md
@@ -0,0 +1,10 @@
+## 1 字符设备
++ register_chrdev_region
++ alloc_chrdev_region
++ unregister_chrdev_region
+
++ cdev_init
++ cdev_add
++ cdev_del
+
++ container_of 
--- globalmem/globalmem.c
+++ globalmem/globalmem.c
@@ -8,6 +8,7 @@
 #define GLOBALMEM_SIZE 0x1000
 #define MEM_CLEAR 0x1
 #define GLOBALMEM_MAJOR 230
+#define DEVICE_NUM 10
 
 static int globalmem_major =GLOBALMEM_MAJOR;
 module_param(globalmem_major, int, S_IRUGO);
@@ -21,7 +22,8 @@ struct globalmem_dev *globalmem_devp;
 
 static int globalmem_open(struct inode* inode, struct file* filp)
 {
-    filp->private_data = globalmem_devp;
+    struct globalmem_dev* dev = container_of(inode->i_cdev, struct globalmem_dev, cdev);
+    filp->private_data = dev;
     return 0;
 }
 
@@ -47,7 +49,7 @@ static long globalmem_ioctl(struct file *filp, unsigned int cmd, unsigned long a
     return 0;
 }
 
-static ssize_t globalmem_read(struct file* flip, char __user * buf, size_t size, loff_t* ppos)
+static ssize_t globalmem_read(struct file* filp, char __user * buf, size_t size, loff_t* ppos)
 {
     unsigned long p = *ppos;
     unsigned int count = size;
@@ -122,17 +124,17 @@ static loff_t globalmem_llseek(struct file* filp, loff_t offset, int orig)
             break;
         // start of cur pos
         case 1:
-            if ((filp->fpos + offset) > GLOBALMEM_SIZE)
+            if ((filp->f_pos + offset) > GLOBALMEM_SIZE)
             {
                 ret = -EINVAL;
                 break;
             }
-            if ((filp->fpos + offset) < 0)
+            if ((filp->f_pos + offset) < 0)
             {
                 ret = -EINVAL;
                 break;
             }
-            filp->fpos += offset;
+            filp->f_pos += offset;
             ret = filp->f_pos;
             break;
         default:
@@ -152,7 +154,7 @@ static const struct file_operations globalmem_fops = {
     .release = globalmem_release,
 };
 
-static void globalmem_setop_cdev(struct globalmem_dev* dev, int index)
+static void globalmem_setup_cdev(struct globalmem_dev* dev, int index)
 {
     int err, devno = MKDEV(globalmem_major, index);
     
@@ -167,32 +169,34 @@ static void globalmem_setop_cdev(struct globalmem_dev* dev, int index)
 static int __init globalmem_init(void)
 {
     int ret;
+    int i;
     dev_t devno = MKDEV(globalmem_major, 0);
 
     if (globalmem_major)
     {
-        ret = register_chrdev_region(devno, 1, "globalmem");
+        ret = register_chrdev_region(devno, DEVICE_NUM, "globalmem");
     }
     else
     {
-        ret = alloc_chrdev_region(&devno, 0, 1, "globalmem");
+        ret = alloc_chrdev_region(&devno, 0, DEVICE_NUM, "globalmem");
         globalmem_major = MAJOR(devno);
     }
 
-    if (ret < ) return ret;
+    if (ret < 0) return ret;
 
-    globalmem_devp = kzalloc(sizeof(struct globalmem_dev), GFP_KERNEL);
+    globalmem_devp = kzalloc(sizeof(struct globalmem_dev) * DEVICE_NUM, GFP_KERNEL);
     if (!globalmem_devp)
     {
         ret = -ENOMEM;
         goto fail_malloc;
     }
 
-    globalmem_setup_cdev(globalmem_devp, 0);
+    for (i = 0; i < DEVICE_NUM; i++)
+        globalmem_setup_cdev(globalmem_devp + i, i);
     return 0;
 
 fail_malloc:
-    unregister_chrdev_region(devno, 1);
+    unregister_chrdev_region(devno, DEVICE_NUM);
     return ret;
 }
 
@@ -200,9 +204,11 @@ module_init(globalmem_init);
 
 static void __exit globalmem_exit(void)
 {
-    cdev_del(&globalmem_devp->cdev);
+    int i;
+    for (i = 0; i < DEVICE_NUM; i++)
+        cdev_del(&(globalmem_devp + i)->cdev);
     kfree(globalmem_devp);
-    unregister_chrdev_region((MKDEV(globalmem_major, 0), 1));
+    unregister_chrdev_region(MKDEV(globalmem_major, 0), DEVICE_NUM);
 }
 
 module_exit(globalmem_exit);
